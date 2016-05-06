0cb2e62a2e7ee1ddf6299c4e7567bf4b183c7562
Stavros Kounis <skounis@gmail.com>
2016-05-06 10:56:41 +0300

    feat(*) Add move object example

--- app/scripts/canvas-move/canvas-move.controller.js
+++ app/scripts/canvas-move/canvas-move.controller.js
@@ -10,16 +10,16 @@
 	/* @ngInject */
 	function CanvasMoveController($scope, canvasMoveService) {
 		var vm = angular.extend(this, {
-			path: null,
-			rect: null,
-			startPoint: null,
-			rectShape: null,
 			tool: null,
+			project: null,
+			segment: null,
+			path: null,
+			movePath: false,
+			values: null,
+			hitOptions: null,
 			onMouseDown: onMouseDown,
 			onMouseDrag: onMouseDrag,
 			onMouseUp: onMouseUp,
-			rects: [],
-			shapes: [],
 		});
 
 		// Initialize
@@ -38,81 +38,126 @@
 				fillColor: 'black',
 			});
 
-			// Sample draws
-			drawSampleRects();
-
 			paper.view.update();
 
 			vm.tool = new Tool();
+			vm.project = paper.project;
+
+
+			vm.values = {
+				paths: 50,
+				minPoints: 5,
+				maxPoints: 15,
+				minRadius: 30,
+				maxRadius: 90
+			};
+
+			vm.hitOptions = {
+				segments: true,
+				stroke: true,
+				fill: true,
+				tolerance: 5
+			};
+
+			createPaths();
 
 			// Define a mousedown and mousedrag handler
 			vm.tool.onMouseDown 	= onMouseDown;
 			vm.tool.onMouseDrag 	= onMouseDrag;
+			vm.tool.onMouseMove   = onMouseMove;
 			vm.tool.onMouseUp 		= onMouseUp;
 
+		} // initPaper
 
-    } // initPaper
 
-		function resetShapes(){
-			for (var i in vm.shapes) {
-					vm.shapes[i].strokeColor = 'green';
+		function createPaths() {
+			var radiusDelta = vm.values.maxRadius - vm.values.minRadius;
+			var pointsDelta = vm.values.maxPoints - vm.values.minPoints;
+
+			for (var i = 0; i < vm.values.paths; i++) {
+				var radius = vm.values.minRadius + Math.random() * radiusDelta;
+				var points = vm.values.minPoints + Math.floor(Math.random() * pointsDelta);
+				var p = Point.random();
+				p.x = p.x * paper.view.size.width;
+				p.y = p.y * paper.view.size.height;
+				// var path = createBlob(paper.view.size * Point.random(), radius, points);
+				var path = createBlob(p, radius, points);
+				var lightness = (Math.random() - 0.5) * 0.4 + 0.4;
+				var hue = Math.random() * 360;
+				path.fillColor = { hue: hue, saturation: 1, lightness: lightness };
+				path.strokeColor = 'black';
+			};
+		}
+
+		function createBlob(center, maxRadius, points) {
+			var path = new Path();
+			path.closed = true;
+			for (var i = 0; i < points; i++) {
+				var delta = new Point({
+					length: (maxRadius * 0.5) + (Math.random() * maxRadius * 0.5),
+					angle: (360 / points) * i
+				});
+				// path.add(center + delta);
+				var p = new Point(center.x + delta.x, center.y + delta.y)
+				path.add(p);
 			}
-		}
-
-		function drawRect(startPoint, endPoint){
-			var rect = new Rectangle(startPoint, endPoint);
-			vm.rects.push(rect);
-			var rectShape = new Shape.Rectangle(rect);
-			rectShape.strokeColor = 'green';
-			vm.shapes.push(rectShape);
-		}
-
-		function drawSampleRects(){
-			var start = new Point(10, 50);
-			var end = new Point(150, 100);
-			drawRect(start, end);
-
-			start = new Point(paper.view.size.width / 2, 150);
-			end = new Point(paper.view.size.width / 2 + 150, 250);
-			drawRect(start, end);
-
-			start = new Point(paper.view.size.width / 2 - 100, 350);
-			end = new Point(paper.view.size.width - 50, 450);
-			drawRect(start, end);
-
-			console.log();
+			path.smooth();
+			return path;
 		}
 
 		function onMouseDown(event) {
-			vm.startPoint = event.point;
-			resetShapes();
-		}
+			vm.segment = vm.path = null;
+			var hitResult = project.hitTest(event.point, vm.hitOptions);
+			if (!hitResult)
+				return;
 
-		// While the user drags the mouse, points are added to the path
-		// at the position of the mouse:
-		function onMouseDrag(event) {
-
-			if (vm.rectShape){
-				vm.rectShape.remove();
+			if (event.modifiers.shift) {
+				if (hitResult.type == 'segment') {
+					hitResult.segment.remove();
+				};
+				return;
 			}
 
-			vm.rect = new Rectangle(vm.startPoint, event.point);
-			vm.rectShape = new Shape.Rectangle(vm.rect);
-			vm.rectShape.strokeColor = 'black';
+			if (hitResult) {
+				vm.path = hitResult.item;
+				if (hitResult.type == 'segment') {
+					vm.segment = hitResult.segment;
+				} else if (hitResult.type == 'stroke') {
+					var location = hitResult.location;
+					vm.segment = path.insert(location.index + 1, event.point);
+					path.smooth();
+				}
+			}
+			vm.movePath = hitResult.type == 'fill';
+			if (vm.movePath)
+				project.activeLayer.addChild(hitResult.item);
 		}
+
+		function onMouseMove(event) {
+			vm.project.activeLayer.selected = false;
+			if (event.item)
+				event.item.selected = true;
+		}
+
+		function onMouseDrag(event) {
+			if (vm.segment) {
+				vm.segment.point.x = vm.segment.point.x  + event.delta.x;
+				vm.segment.point.y = vm.segment.point.y  + event.delta.y;
+
+				vm.path.smooth();
+				console.log('segment point:', vm.segment.point);
+			} else if (vm.path) {
+				// vm.path.position += event.delta;
+				vm.path.position.x =  vm.path.position.x + event.delta.x;
+				vm.path.position.y =  vm.path.position.y + event.delta.y;
+				console.log('path position:', vm.path.position);
+			}
+		}
+
 
 		// When the mouse is released, we simplify the path:
 		function onMouseUp(event) {
-			// alert('Select area: ' + JSON.stringify(vm.rect));
-			for (var i in vm.rects) {
-				if (vm.rect.intersects(vm.rects[i])){
-					vm.shapes[i].strokeColor = 'red';
-					// alert('Intersects with: ' + JSON.stringify(vm.rects[i]));
-					console.log('Intersects with: ', vm.rects[i]);
-				}
-			}
-			console.log(vm.rect);
-		}
 
+		}
 	}
 })();
